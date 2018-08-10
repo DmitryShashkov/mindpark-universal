dd719d5bcc039b301a378ab432b4c1ff2230c97e
Egor Zhukov <egor.zhukov.cr@gmail.com>
2018-08-10 16:10:07 +0300

    Downgrade from 5 to angular 4

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -2,8 +2,8 @@ import {NgModule} from '@angular/core';
 
 import {AppModule} from './app.module';
 import {AppComponent} from './app.component';
-import {ServerModule, ServerTransferStateModule} from "@angular/platform-server";
-import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader';
+import {ServerModule} from "@angular/platform-server";
+//import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader';
 
 @NgModule({
     imports: [
@@ -11,8 +11,8 @@ import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader';
         // by the ServerModule from @angular/platform-server.
         AppModule,
         ServerModule,
-        ModuleMapLoaderModule,
-        ServerTransferStateModule,
+        // ModuleMapLoaderModule,
+        // ServerTransferStateModule,
     ],
     // Since the bootstrapped component is not inherited from your
     // imported AppModule, it needs to be repeated here.
