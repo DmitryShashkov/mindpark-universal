e941cbc1a476b53700cd493cd534d149c306a324
shashkov.cr <shashkov.cr@gmail.com>
2018-08-13 12:14:42 +0300

    Added missing deployURL

--- src/app/brexit/brexit.component.ts
+++ src/app/brexit/brexit.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {environment} from "../../environments/environment";
 
 @Component({
     selector: 'brexit',
@@ -6,5 +7,5 @@ import {Component} from "@angular/core";
     styleUrls: ['./brexit.component.css']
 })
 export class BrexitComponent {
-
+    public readonly deployURL: string = environment.deployURL;
 }
